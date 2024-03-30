3f39249b0d7c17c97a9a8ab79cc1da201fd30046
Aaron Post <aarontpost@gmail.com>
2024-03-30 15:35:06 -0400

    Bedroom Amber Routine

---
+++ Assets/Scripts/AI/Bedroom/AmberBedroomBT.cs
@@ -0,0 +1,33 @@
+using Assets.Scripts.AI.Kitchen;
+using BehaviorTree;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Scripts.AI
+{
+    public class AmberBedroomBT : BehaviorTree.Tree
+    {
+        [SerializeField]
+        grid_manager grid;
+        [SerializeField]
+        InteractableObject alarmClockClick, bookClick;
+        [SerializeField]
+        AmberMount sitInBed, navigation;
+        [SerializeField]
+        tile _bathroomDoor;
+        protected override Node SetupTree()
+        {
+            Node routine = new Sequence(new List<Node>()
+            {
+                new WaitForPlayerInteractionCompleted(alarmClockClick), 
+                new SwitchAmberMount(sitInBed), 
+                new WaitForPlayerInteractionCompleted(bookClick),
+                new SwitchAmberMount(navigation),
+                new MoveToTile(grid, _bathroomDoor),
+                //new AmberMoveToRoom("Bathroom"),
+            });
+            return routine;
+        }
+    }
+}
---
+++ Assets/Scripts/AI/GeneralNodes/AmberMount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.AI
+{
+    internal class AmberMount : MonoBehaviour
+    {
+        public Action CompletedMounting;
+        [SerializeField] private readonly GameObject _mount;
+        [SerializeField] private readonly bool _enabledOnLoad = false;
+        void Awake() {
+            _mount.SetActive(_enabledOnLoad);
+        }
+        public void Mount() {
+            StartCoroutine("MountWithDelay");
+        }
+        private IEnumerator MountWithDelay() {
+            yield return new WaitForSeconds(0.3f);
+            _mount.SetActive(true);
+            foreach (AmberMount mount in FindObjectsOfType<AmberMount>())
+            {
+                if (mount != this)
+                {
+                    mount.UnMount();
+                }
+            }
+            yield return new WaitForSeconds(0.3f);
+            CompletedMounting?.Invoke();
+        }
+        public void UnMount() {
+            _mount.SetActive(false);
+        }
+    }
+}
