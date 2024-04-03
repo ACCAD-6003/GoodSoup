de125af0078982b9db0653fa9d58aeaa0f0271c3
Aaron Post <aarontpost@gmail.com>
2024-04-02 22:45:31 -0400

    Bathroom almost done

---
+++ Assets/Scripts/AI/GeneralNodes/AmberHairBrush.cs
@@ -0,0 +1,21 @@
+﻿using BehaviorTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.AI.GeneralNodes
+{
+    public class AmberHairBrush : Node
+    {
+        bool hairBrushed = false;
+        public override NodeState Evaluate() {
+            if (!hairBrushed) {
+                hairBrushed = true;
+                // Plug Amber Hair Changing logic into here
+            }
+            return NodeState.SUCCESS;
+        }
+    }
+}
