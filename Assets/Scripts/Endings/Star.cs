edb80751906e484c812a69d9067127f27296c352
Aaron Post <aarontpost@gmail.com>
2024-04-18 18:56:23 -0400

    Added package + star animation

---
+++ Assets/Scripts/Endings/Star.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Star : MonoBehaviour
+{
+    public Image YellowStar;
+
+    private void Awake()
+    {
+        YellowStar.transform.localScale = Vector3.zero;
+    }
+}
