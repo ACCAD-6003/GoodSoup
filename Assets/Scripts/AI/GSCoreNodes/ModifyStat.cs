f553c351ea14a35bc197941f4060d0d1f5d65595
Aaron Post <aarontpost@gmail.com>
2024-05-14 13:31:15 -0400

    Bathroom refactor almost done

--- Assets/Scripts/AI/GSCoreNodes/ModifyStat.cs
+++ Assets/Scripts/AI/GSCoreNodes/ModifyStat.cs
@@ -6,7 +6,7 @@ namespace Assets.Scripts.AI.GeneralNodes
     public class ModifyStat : IEvaluateOnce
     {
         public StoryDataType _data;
-        private float _value;
+        public float _value;
         public override void Run() {
             var prevStatValue = ((StoryData<float>) Instance.GetStoryDataValue(_data)).Value;
             Instance.SetStoryDataValue(_data, prevStatValue + _value + "");
