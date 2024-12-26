29c8c2685c507733b173bffbba6322301e17778b
Aaron Post <aarontpost@gmail.com>
2024-12-26 15:36:34 -0600

    Fixes

--- Assets/Goback.cs
+++ Assets/Goback.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 using static MainSceneLoading;
 public class Goback : MonoBehaviour
 {
+    bool goingBack = false;
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space)) {
-            MainSceneLoading.Instance.SwitchAmberRooms(AmberRoom.HALLWAY);
+        if (Input.GetKey(KeyCode.Space) && !goingBack) {
+			goingBack = true;
+			MainSceneLoading.Instance.SwitchAmberRooms(AmberRoom.HALLWAY);
         }
     }
 }
