368dae572e12df24782d16e24ee09357f304666b
Aaron Post <aarontpost@gmail.com>
2024-12-31 14:35:09 -0600

    More sounds, sound levels adjusted, added faders, ui adjustments

--- Assets/Scripts/UI/ChangeScene.cs
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+	public void SwitchSceneAfterDelay(string sceneName)
+	{
+		StartCoroutine(PerformSwitchAfterTime(sceneName, 1f));
+	}
+	private IEnumerator PerformSwitchAfterTime(string sceneName, float seconds) { 
+        yield return new WaitForSeconds(seconds);
+		SwitchScene(sceneName);
+	}
     public void SwitchScene(string sceneName) {
         if (EndingSetup.timesBeaten == 9) {
             StoryDatastore.Instance.ChosenEnding.Value = Ending.TOUCH_GRASS;
